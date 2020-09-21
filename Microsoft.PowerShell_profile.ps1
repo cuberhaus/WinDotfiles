73051d18d1f9df342c2750598fc6086a498548a5
Pol Casacuberta Gil <polcg@hotmail.es>
2020-09-21 22:19:10 +0200

    powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+﻿Function Test-Elevated {
+    $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
+    $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $prp.IsInRole($adm)
+}
+function update {
+    choco update all
+    Get-WindowsUpdate
+    Install-WindowsUpdate
+}
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
