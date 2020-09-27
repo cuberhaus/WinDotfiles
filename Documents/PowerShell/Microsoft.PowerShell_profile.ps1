ec42de0d1b810f5e3ce2b0d42a352d5484919bc2
Pol Casacuberta Gil <polcg@hotmail.es>
2020-09-27 18:06:14 +0200

    Added powershell 7 config

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,57 @@
+﻿function Test-Elevated {
+    $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
+    $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $prp.IsInRole($adm)
+}
+function update {
+    choco upgrade all
+    Get-WindowsUpdate
+    Install-WindowsUpdate
+}
+function o {
+    explorer .
+}
+function .. {
+    cd ..
+}
+function ... {
+    cd ../..
+}
+function .... {
+    cd ../../..
+}
+function ..... {
+    cd ../../../..
+}
+function c {
+    clear
+}
+function g {
+    git $args
+}
+function vim {
+    nvim $args
+}
+
+Import-Module git-aliases -DisableNameChecking
+
+function ga {
+    git add $args
+}
+function gf {
+    git fetch $args
+}
+function gm {
+    git merge $args
+}
+function gp {
+    git push $args
+}
+function gs {
+    git status $args
+}
+function gsu {
+    git submodule update --remote --recursive $args
+}
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# WinDotfiles
+# Windows 10 Dotfiles
+### PowerShell
+- Powershell 5
+C:\Users\polcg\Documents\WindowsPowerShell
+- Powershell 7
+C:\Users\polcg\Documents\PowerShell
