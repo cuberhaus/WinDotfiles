0a6d39baeee67f9d746a5571a94f8e80c1bfe1d3
cuberhaus <pol@gmail.com>
2020-12-13 00:01:02 +0100

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

---
+++ bin/ycm.ps1
@@ -0,0 +1,10 @@
+ycm_dependancies # Installs dependancies
+cd ~\.vim\plugged\YouCompleteMe\
+python3 install.py --all
+
+function ycm_dependancies {
+    choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' # cmake
+    choco install golang
+    choco install nodejs # npm
+    choco install visualstudio2017buildtools
+}
