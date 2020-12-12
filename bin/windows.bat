8abaa4d90558cf0bea3690ee4b18b1252226fcf7
cuberhaus <pol@gmail.com>
2020-12-12 23:06:00 +0100

    I don't get paid enough for this shit.

--- bin/windows.bat
+++ bin/windows.bat
@@ -1,5 +1,5 @@
 Rem uncap location
-set uncap=~\WinDotfiles\dotfiles\uncap.exe 
+set uncap=~\WinDotfiles\dotfiles\uncap.exe
 
 CALL :windows_install
 CALL :vim_install
@@ -7,7 +7,7 @@ cd "~\dotfiles\.local\bin\
 CALL ycm.bat
 CALL :swap
 REM CALL :games_install
-EXIT /B %ERRORLEVEL% 
+EXIT /B %ERRORLEVEL%
 
 :vim_install
 choco install vim -y
@@ -18,7 +18,7 @@ ni "$env:LOCALAPPDATA/nvim-data/site/autoload/plug.vim" -Force
 
 Rem vim plug for PowerShell
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni $HOME/vimfiles/autoload/plug.vim -Force
+ni $HOME/vimfiles/autoload/plug.vim -Force
 
 npm install -g neovim Rem npm
 
@@ -33,57 +33,58 @@ Rem Swap caps with escape https://github.com/susam/uncap#readme
 EXIT /B 0
 
 :windows_install
-Install-Module git-aliases -Scope CurrentUser -AllowClobber Rem https://github.com/gluons/powershell-git-aliases
+Install-Module git-aliases -Scope CurrentUser -AllowClobber
+Rem https://github.com/gluons/powershell-git-aliases
 Install-Module PSWindowsUpdate
 Get-WindowsUpdate
 Install-WindowsUpdate
 
-Rem chocolatey 
+Rem chocolatey, do NOT add comments to choco
 
-choco install 7zip.install -y   Rem Archiver
-choco install adobereader -y 	Rem Pdf viewer
-choco install atom -y           Rem GUI Editor
-choco install audacity          Rem Audio editor
-choco install autohotkey.portable -y Rem Automation software
-choco install calibre -y        Rem Books manager
-choco install ccleaner -y 	    Rem Cleanup
-choco install curl -y 		    Rem cUrL is a command line tool and library for transferring data with URLs
-choco install discord -y        Rem Discord
-choco install fzf -y            Rem Fuzzy finder
-choco install gimp -y           Rem Photoshop
-choco install git.install -y    Rem Git
-choco install googlechrome -y	Rem Web browser
-choco install intellijidea-community Rem Free version java IDE
-choco install intellijidea-ultimate Rem Paid version with sql IDE
-choco install linkshellextension -y Rem Make links from explorer
-choco install malwarebytes -y   Rem Anti-virus
-choco install microsoft-windows-terminal -y	Rem Windows terminal
-choco install obs-studio -y     Rem Record screen in windows, works with internal audio better than mac
-choco install openjdk           Rem open source java development kit
-choco install openssh -y	    Rem SSH client
-choco install poshgit -y        Rem git bar
-choco install powershell-core -y Rem Updated powershell
-choco install python -y         
-choco install r.project -y      Rem Probabilitat i estadística
-choco install r.studio -y       Rem Probabilitat i estadística IDE
-choco install rufus -y          Rem burn iso's on usb
-choco install skype -y		    Rem Skype
-choco install spotify -y        Rem Spotify
+choco install 7zip.install -y
+choco install adobereader -y
+choco install atom -y
+choco install audacity
+choco install autohotkey.portable -y
+choco install calibre -y
+choco install ccleaner -y
+choco install curl -y
+choco install discord -y
+choco install fzf -y
+choco install gimp -y
+choco install git.install -y
+choco install googlechrome -y
+Rem choco install intellijidea-community -y
+choco install intellijidea-ultimate-y
+choco install linkshellextension -y
+choco install malwarebytes -y
+choco install microsoft-windows-terminal -y
+choco install obs-studio -y
+choco install openjdk -y
+choco install openssh -y
+choco install poshgit -y
+choco install powershell-core -y
+choco install python -y
+choco install r.project -y
+choco install r.studio -y
+choco install rufus -y
+choco install skype -y
+choco install spotify -y
 choco install strawberryperl -y
-choco install telegram -y       Rem Telegram
-choco install thunderbird -y    Rem Email client
-choco install toastify -y 		Rem Toastify adds some missing functionallity to the Spotify client.
-choco install transmission -y   Rem Simple torrent client
-choco install vlc -y		    Rem Media player
-choco install vscode -y         Rem GUI Editor
-choco install wget -y		    Rem A command-line utility for retrieving files using HTTP protocols
+choco install telegram -y
+choco install thunderbird -y
+choco install toastify -y
+choco install transmission -y
+choco install vlc -y
+choco install vscode -y
+choco install wget -y
 choco install windirstat -y
-choco install wsl2 -y           Rem Windows subsystem for linux 2
-choco install yarn -y           Rem Packages
+choco install wsl2 -y
+choco install yarn -y
 
-Rem choco install autoruns -y		Rem What programs are configured to startup automatically
+Rem choco install autoruns -y       Rem What programs are configured to startup automatically
 Rem choco install chocolateygui -y  Rem A gui for chocolatey package manager
-Rem choco install chromium -y 	    Rem Open source Web browser
+Rem choco install chromium -y       Rem Open source Web browser
 Rem choco install eclipse -y        Rem java/SQL IDE // doesnt quite work properly??
 Rem choco install firefox -y        Rem Open source web browser
 Rem choco install git-lfs -y
