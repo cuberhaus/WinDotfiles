424ed85bd8569cd3b04b5741064a55a536cd7795
cuberhaus <polcg10@gmail.com>
2023-02-22 11:09:27 +0100

    prepended -y so that you can easily spot apps without it

--- bin/windows.ps1
+++ bin/windows.ps1
@@ -44,23 +44,23 @@ function base_install {
     # Chocolatey install
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-    choco install 7zip.install -y   # Archiver
-    choco install curl -y 		    # Curl is a command line tool and library for transferring data with URLs
-    choco install fzf -y            # Fuzzy finder
-    choco install git.install -y    # Git
-    choco install googlechrome -y	# Web browser
-    choco install linkshellextension -y # Make links from explorer
-    choco install make -y           # makefiles
-    choco install nirlauncher -y    # have gui for sysinternals (MUST GO FIRST)
-    choco install openssh -y	    # SSH client
-    choco install poshgit -y        # git bar
-    choco install powershell-core -y # Updated powershell
-    choco install python -y         # python
-    choco install rclone -y         # Git but without version control
-    choco install strawberryperl -y # Pearl
-    choco install wget -y		    # A command-line utility for retrieving files using HTTP protocols
-    choco install yarn -y           # Packages, need it for vim
-    choco install zip -y            # zip from terminal
+    choco install -y 7zip.install    # Archiver
+    choco install -y curl  		    # Curl is a command line tool and library for transferring data with URLs
+    choco install -y fzf             # Fuzzy finder
+    choco install -y git.install     # Git
+    choco install -y googlechrome 	# Web browser
+    choco install -y linkshellextension  # Make links from explorer
+    choco install -y make            # makefiles
+    choco install -y nirlauncher     # have gui for sysinternals (MUST GO FIRST)
+    choco install -y openssh 	    # SSH client
+    choco install -y poshgit         # git bar
+    choco install -y powershell-core  # Updated powershell
+    choco install -y python          # python
+    choco install -y rclone          # Git but without version control
+    choco install -y strawberryperl  # Pearl
+    choco install -y wget 		    # A command-line utility for retrieving files using HTTP protocols
+    choco install -y yarn            # Packages, need it for vim
+    choco install -y zip             # zip from terminal
     Install-Module git-aliases -Scope CurrentUser -AllowClobber
 }
 
@@ -76,83 +76,83 @@ function emacs {
     # Add environment variable HOME to C:\Users\polcg\
     # Add $HOME\.emacs.d\bin\doom to path
     # personal config not working yet on windows
-    # choco install emacs -y          # Editor
-    # choco install git ripgrep llvm fd hunspell.portable -y
+    # choco install -y emacs           # Editor
+    # choco install -y git ripgrep llvm fd hunspell.portable 
 }
 
 function full_install {
     pip3 install pipenv
-    choco install obsidian -y       # Readme editor
-    choco install autohotkey.portable -y # Automation software
-    choco install calibre -y        # Books manager
-    choco install ccleaner -y 	    # Cleanup
-    choco install choco-cleaner -y  # Delete caches from chocolatey
-    choco install discord -y        # Discord
-    choco install geforce-experience -y # Nvidia card updates
-    choco install gimp -y           # Photoshop
-    choco install malwarebytes -y   # Anti-virus
-    choco install mobaxterm -y        # PAR (makes wxparaver WORK)
-    choco install openjdk -y        # open source java development kit
-    choco install powertoys -y      # Powertoys!
-    choco install rufus -y          # burn iso's on usb
-    choco install spotify -y        # Spotify
-    choco install sysinternals --params "/Sysinternals" -y # tools for windows
-    choco install thunderbird -y    # Email client
-    choco install transmission -y   # Simple torrent client
-    choco install treesizefree -y     # view file sizes in system that clog memory
-    choco install vlc -y		    # Media player
-    choco install vscode -y         # GUI Editor
-    choco install greenshot -y      # better screenshots
-    # choco install autoruns -y       # What programs are configured to startup automatically
+    choco install -y obsidian        # Readme editor
+    choco install -y autohotkey.portable  # Automation software
+    choco install -y calibre         # Books manager
+    choco install -y ccleaner  	    # Cleanup
+    choco install -y choco-cleaner   # Delete caches from chocolatey
+    choco install -y discord         # Discord
+    choco install -y geforce-experience  # Nvidia card updates
+    choco install -y gimp            # Photoshop
+    choco install -y malwarebytes    # Anti-virus
+    choco install -y mobaxterm         # PAR (makes wxparaver WORK)
+    choco install -y openjdk         # open source java development kit
+    choco install -y powertoys       # Powertoys!
+    choco install -y rufus           # burn iso's on usb
+    choco install -y spotify         # Spotify
+    choco install -y sysinternals --params "/Sysinternals"  # tools for windows
+    choco install -y thunderbird     # Email client
+    choco install -y transmission    # Simple torrent client
+    choco install -y treesizefree      # view file sizes in system that clog memory
+    choco install -y vlc 		    # Media player
+    choco install -y vscode          # GUI Editor
+    choco install -y greenshot       # better screenshots
+    # choco install -y autoruns        # What programs are configured to startup automatically
 }
 
 function optional {
     # Probably
-    # choco install pswindowsupdate -y
-    # choco install windirstat -y     # View file sizes in system to clean up space
-    choco install adobereader -y 	# Pdf viewer
-    choco install audacity -y       # Audio editor
-    choco install chocolateygui -y  # A gui for chocolatey package manager
-    choco install chromium -y       # Open source Web browser
-    choco install cpu-z.install -y  # list pc infor
-    choco install doxygen.install -y  # c++ documentation
-    choco install eclipse -y        # java/SQL IDE // doesnt quite work properly??
-    choco install firefox -y        # Open source web browser
-    choco install git-lfs -y
-    choco install intellijidea-community -y# Free version java IDE
-    choco install intellijidea-ultimate -y # Paid version with sql IDE
-    choco install jdk8 -y           # java v8
-    choco install jre8 -y
-    choco install libreoffice-still -y # Office suite
-    choco install obs-studio -y     # Record screen in windows, works with internal audio better than mac
-    choco install openjdk -y        # open source java development kit
-    choco install pandoc -y         # universal document converter
-    choco install procexp -y        # process explorer
-    choco install putty -y
-    choco install pycharm -y        # Best Python IDE
-    choco install pycharm-community -y # Community version
-    choco install r.project -y      # Probabilitat i estadística
-    choco install r.studio -y       # Probabilitat i estadística IDE
-    choco install skype -y		    # Skype
-    choco install slack -y          # Slack
-    choco install sublimetext3 -y   # fast editor
-    choco install teamviewer -y
-    choco install telegram -y       # Telegram
-    choco install toastify -y 		# Toastify adds some missing functionallity to the Spotify client.
-    choco install tor-browser -y    # browse the web without restrictions and without being traced
-    choco install virtualbox -y     # Virtualization tool
-    choco install wireshark -y
+    # choco install -y pswindowsupdate 
+    # choco install -y windirstat      # View file sizes in system to clean up space
+    choco install -y adobereader  	# Pdf viewer
+    choco install -y audacity        # Audio editor
+    choco install -y chocolateygui   # A gui for chocolatey package manager
+    choco install -y chromium        # Open source Web browser
+    choco install -y cpu-z.install   # list pc infor
+    choco install -y doxygen.install   # c++ documentation
+    choco install -y eclipse         # java/SQL IDE // doesnt quite work properly??
+    choco install -y firefox         # Open source web browser
+    choco install -y git-lfs 
+    choco install -y intellijidea-community # Free version java IDE
+    choco install -y intellijidea-ultimate  # Paid version with sql IDE
+    choco install -y jdk8            # java v8
+    choco install -y jre8 
+    choco install -y libreoffice-still  # Office suite
+    choco install -y obs-studio      # Record screen in windows, works with internal audio better than mac
+    choco install -y openjdk         # open source java development kit
+    choco install -y pandoc          # universal document converter
+    choco install -y procexp         # process explorer
+    choco install -y putty 
+    choco install -y pycharm         # Best Python IDE
+    choco install -y pycharm-community  # Community version
+    choco install -y r.project       # Probabilitat i estadística
+    choco install -y r.studio        # Probabilitat i estadística IDE
+    choco install -y skype 		    # Skype
+    choco install -y slack           # Slack
+    choco install -y sublimetext3    # fast editor
+    choco install -y teamviewer 
+    choco install -y telegram        # Telegram
+    choco install -y toastify  		# Toastify adds some missing functionallity to the Spotify client.
+    choco install -y tor-browser     # browse the web without restrictions and without being traced
+    choco install -y virtualbox      # Virtualization tool
+    choco install -y wireshark 
     
     # Garbage
-    # choco install python2 -y // THIS BREAKS NEOVIM PYTHON
-    # choco install reflect-free -y   # backups (EOL) just use windows (crear una imagen de sistema)
-    # choco install veeam-agent -y    # backups This DESTROYED MY PC
-    # choco install wsl2 -y           # Windows subsystem for linux 2
+    # choco install -y python2  // THIS BREAKS NEOVIM PYTHON
+    # choco install -y reflect-free    # backups (EOL) just use windows (crear una imagen de sistema)
+    # choco install -y veeam-agent     # backups This DESTROYED MY PC
+    # choco install -y wsl2            # Windows subsystem for linux 2
 }
 
 function vim_install {
-    choco install vim -y
-    choco install neovim -y
+    choco install -y vim 
+    choco install -y neovim 
     #vim plug neovim
     iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$env:LOCALAPPDATA/nvim-data/site/autoload/plug.vim" -Force
@@ -168,7 +168,7 @@ function vim_install {
 }
 
 function swap {
-    choco install sharpkeys # allows to take a look at this registry entries and add more keybindings easily
+    choco install -y sharpkeys # allows to take a look at this registry entries and add more keybindings easily
     # better to just use register
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 00000000000000000200000001003a0000000000
     # cp $uncap C:\Windows\
@@ -176,10 +176,10 @@ function swap {
 }
 
 function games_install {
-    choco install goggalaxy -y
-    choco install leagueoflegends -y
-    choco install steam -y
-    choco install epicgameslauncher -y
+    choco install -y goggalaxy 
+    choco install -y leagueoflegends 
+    choco install -y steam 
+    choco install -y epicgameslauncher 
 }
 
 function tasks {
