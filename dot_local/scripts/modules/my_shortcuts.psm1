91254b56915dc5434400bfe7a144693171196291
cuberhaus <polcg10@gmail.com>
2023-02-27 16:06:33 +0100

    This is a placeholder

--- dot_local/scripts/modules/my_shortcuts.psm1
+++ dot_local/scripts/modules/my_shortcuts.psm1
@@ -1,7 +1,11 @@
 # Functions here should have no logic, just shortcuts to other functions
+# These should also not need any documentation
 function aliases {
     # code $dotfiles\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
     code $dotfiles\Documents\PowerShell\profile.ps1
+    code $modules\my_git.psm1
+    code $modules\my_rclone.psm1
+    code $modules\my_shortcuts.psm1
 }
 function vimrc {
     vim $dotfiles\_vimrc
