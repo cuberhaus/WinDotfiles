67d4c203df24e9b3ce27deb9f06d17a323d3be37
cuberhaus <polcg10@gmail.com>
2023-03-01 20:40:50 +0100

    better shortcuts

--- home/dot_local/scripts/modules/my_shortcuts.psm1
+++ home/dot_local/scripts/modules/my_shortcuts.psm1
@@ -2,19 +2,16 @@
 # These should also not need any documentation
 function aliases {
     # code $dotfiles\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-    code $dotfiles\Documents\PowerShell\profile.ps1
-    code $modules\my_git.psm1
-    code $modules\my_rclone.psm1
-    code $modules\my_shortcuts.psm1
+    ce $HOME\Documents\PowerShell\profile.ps1 $modules\my_git.psm1 $modules\my_rclone.psm1 $modules\my_shortcuts.psm1
 }
 function vimrc {
-    cvim $dotfiles\_vimrc
+    ce $dotfiles\_vimrc
 }
 function windows {
-    cvim $scripts\windows.ps1
+    ce $scripts\windows.ps1
 }
 function win {
-    cvim $scripts\windows.ps1
+    ce $scripts\windows.ps1
 }
 # Quick shortcut to start notepad
 function n { notepad $args }
@@ -42,7 +39,7 @@ function vim {
     nvim $args
 }
 # This will open the file in vim and apply the changes to the home directory as well as the source file
-function cvim {
+function ce {
     chezmoi edit --apply $args
 }
 
