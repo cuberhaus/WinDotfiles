da90bc456b33d89cd224d19797c435a6a7cc086c
cuberhaus <polcg10@gmail.com>
2023-03-13 00:43:47 +0100

    added emacs bookmarks

---
+++ home/dot_cache/emacs/var/bookmark-default.el
@@ -0,0 +1,30 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: utf-8-emacs; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("Tasks.org"
+ (filename . "~/repos/docs/org/Tasks.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("Metrics.org"
+ (filename . "~/repos/docs/org/Metrics.org")
+ (front-context-string . "* Weight\n| Date ")
+ (rear-context-string)
+ (position . 1))
+("Journal.org"
+ (filename . "~/repos/docs/org/Journal.org")
+ (front-context-string . "\n\n* 2022\n\n** 202")
+ (rear-context-string)
+ (position . 1))
+("emacs.org"
+ (filename . "~/repos/dotfiles/.config/emacs.org")
+ (front-context-string)
+ (rear-context-string . "list)\n#+end_src\n")
+ (position . 87855))
+("org-capture-last-stored"
+ (filename . "~/repos/docs/org/Metrics.org")
+ (front-context-string . "| [2023-03-13 Mo")
+ (rear-context-string . "ite good      |\n")
+ (position . 310))
+)
