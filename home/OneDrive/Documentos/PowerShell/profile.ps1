17594d5ed8d9fc386edc0fae890dba989005b4d1
cuberhaus <polcg10@gmail.com>
2023-06-18 12:53:16 +0200

    This is a placeholder

--- home/OneDrive/Documentos/PowerShell/profile.ps1
+++ home/OneDrive/Documentos/PowerShell/profile.ps1
@@ -6,15 +6,23 @@ $modules = "$HOME\.local\scripts\modules"
 # https://github.com/ralish/PSDotFiles
 $DotFilesPath = $dotfiles # This is needed
 
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
 Import-Module git-aliases -DisableNameChecking
-Import-Module -Name "$modules\my_git.psm1"
-Import-Module -Name "$modules\my_shortcuts.psm1"
-Import-Module -Name "$modules\my_rclone.psm1"
-Import-Module -Name "$modules\my_tools.psm1"
+Import-Module -DisableNameChecking -Name "$modules\my_git.psm1"
+Import-Module -DisableNameChecking -Name "$modules\my_shortcuts.psm1"
+Import-Module -DisableNameChecking -Name "$modules\my_rclone.psm1"
+Import-Module -DisableNameChecking -Name "$modules\my_tools.psm1"
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
-function l{
-    Get-ChildItem -Force -Color
-}
+
+# ALIASES MUST GO AFTER MODULE IMPORTS AND CAN'T GO INSIDE MODULES
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
@@ -25,3 +33,4 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
+
