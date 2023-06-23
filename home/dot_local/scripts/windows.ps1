53211556aab3e2aa60fb495329808db98ff8deaf
cuberhaus <polcg10@gmail.com>
2023-06-23 13:41:30 +0200

    fixed some of the windows uninstall stuff

--- home/dot_local/scripts/windows.ps1
+++ home/dot_local/scripts/windows.ps1
@@ -249,31 +249,25 @@ function path {
     [Environment]::SetEnvironmentVariable("EDITOR", "code --wait", [EnvironmentVariableTarget]::Machine)
     [Environment]::SetEnvironmentVariable("HOME", "C:\Users\polcg", [EnvironmentVariableTarget]::Machine)
     setx HOME %USERPROFILE%
-    [Environment]::SetEnvironmentVariable("cho", "$cho", [EnvironmentVariableTarget]::Machine)
+    # [Environment]::SetEnvironmentVariable("cho", "$cho", [EnvironmentVariableTarget]::Machine)
 }
 function remove_bloat {
-    Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage
+    Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage # Cortana
     Get-AppxPackage -AllUsers Microsoft.BingWeather | Remove-AppxPackage
+    Get-AppxPackage -AllUsers Microsoft.GetHelp | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppPackage
-    Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppPackage
-    Get-AppxPackage -AllUsers Microsoft.MSPaint | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.Microsoft3DViewer | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.MixedReality.Portal | Remove-AppxPackage
+    Get-AppxPackage -AllUsers Microsoft.People | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.ScreenSketch | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.SkypeApp | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.StorePurchaseApp | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.Windows.Photos | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.WindowsAlarms | Remove-AppxPackage
-    # Get-AppxPackage -AllUsers Microsoft.WindowsCalculator | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.WindowsCamera | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.WindowsCommunicationsApps | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.WindowsFeedbackHub | Remove-AppPackage
     Get-AppxPackage -AllUsers Microsoft.WindowsMaps | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.WindowsStore | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.Xbox.TCUI | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.XboxApp | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.XboxGameOverlay | Remove-AppxPackage
@@ -283,8 +277,6 @@ function remove_bloat {
     Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.ZuneMusic | Remove-AppxPackage
     Get-AppxPackage -AllUsers Microsoft.ZuneVideo | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.People | Remove-AppxPackage
-    Get-AppxPackage -AllUsers Microsoft.GetHelp | Remove-AppxPackage
 }
 function errors {
     chkdsk # This will check disk for errors
